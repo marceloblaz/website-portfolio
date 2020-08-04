bc03a4e6653dfe9e6af9fe2da3480d067e9e3435
marceloblaz <marcelo.brasileirovale@gmail.com>
2020-08-04 17:05:33 -0300

    adicionei um novo icone no componente de detalhes do projeto

--- src/app/projects-service.service.ts
+++ src/app/projects-service.service.ts
@@ -13,8 +13,8 @@ export class ProjectsServiceService {
     this.lista.push(
       new Dado(
         1,
-        "Pagboletos",
-        "descricao projeto 1",
+        "pagboletos",
+        "Descricao projeto 1",
         2020,
         "https://firebasestorage.googleapis.com/v0/b/mb-portfolio-e56f8.appspot.com/o/pagboletos-cover.png?alt=media&token=b8d3e313-b1a8-4a46-a7a1-66bc8bb5033c"
       )
