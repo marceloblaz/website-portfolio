1138cd64aa0d950b38ac30664817e3954229cb4a
marceloblaz <marcelo.brasileirovale@gmail.com>
2020-08-08 15:00:35 -0300

    adicionei lógica para inserir imagens no componente de project details

--- src/app/projects-service.service.ts
+++ src/app/projects-service.service.ts
@@ -17,7 +17,12 @@ export class ProjectsServiceService {
         "An app for paying brazilian boletos with credit card",
         "One of the most popular ways of making payments in Brazil is the so called boleto. It's a bank slip with a barcode that can be scanned in order to make the payment. Normally they must be paid cash, so that's where pagboletos comes in: their proposition value is allowing users to pay the boleto with their credit, which gives the user more time to pay for their debt.",
         2020,
-        "https://firebasestorage.googleapis.com/v0/b/mb-portfolio-e56f8.appspot.com/o/pagboletos-cover.png?alt=media&token=b8d3e313-b1a8-4a46-a7a1-66bc8bb5033c"
+        "https://firebasestorage.googleapis.com/v0/b/mb-portfolio-e56f8.appspot.com/o/pagboletos-cover.png?alt=media&token=b8d3e313-b1a8-4a46-a7a1-66bc8bb5033c",
+        [
+          "https://firebasestorage.googleapis.com/v0/b/mb-portfolio-e56f8.appspot.com/o/pagboletos-cover.png?alt=media&token=b8d3e313-b1a8-4a46-a7a1-66bc8bb5033c",
+          "https://firebasestorage.googleapis.com/v0/b/mb-portfolio-e56f8.appspot.com/o/pagboletos-cover.png?alt=media&token=b8d3e313-b1a8-4a46-a7a1-66bc8bb5033c",           "https://firebasestorage.googleapis.com/v0/b/mb-portfolio-e56f8.appspot.com/o/pagboletos-cover.png?alt=media&token=b8d3e313-b1a8-4a46-a7a1-66bc8bb5033c"
+
+        ]
       )
     );
     this.lista.push(
@@ -25,9 +30,12 @@ export class ProjectsServiceService {
         "2",
         "Buquês são flores mortas",
         "Screen printed poster",
-        "This project was inspired by a song of a brazilian artist, Criolo. It says that bouquets are dead flowers. That sounded very interesting to me, the contrast between the liveness and beauty of a bouquet and the reality that those flowers are actually dead.",
         2017,
-        "endereco da imagem da capa"
+        "https://firebasestorage.googleapis.com/v0/b/mb-portfolio-e56f8.appspot.com/o/pagboletos-cover.png?alt=media&token=b8d3e313-b1a8-4a46-a7a1-66bc8bb5033c",
+        [
+          "https://firebasestorage.googleapis.com/v0/b/mb-portfolio-e56f8.appspot.com/o/pagboletos-cover.png?alt=media&token=b8d3e313-b1a8-4a46-a7a1-66bc8bb5033c",
+          "https://firebasestorage.googleapis.com/v0/b/mb-portfolio-e56f8.appspot.com/o/pagboletos-cover.png?alt=media&token=b8d3e313-b1a8-4a46-a7a1-66bc8bb5033c"
+        ]
       )
     );
   }
--- src/dado.ts
+++ src/dado.ts
@@ -5,12 +5,14 @@ export class Dado {
   longdescription: string;
   ano: number;
   coverurl: any;
-  constructor(id: string, nome: string, description:string, longdescription:string, ano: number, coverurl: string) {
+  imgs: Array<string>;
+  constructor(id: string, nome: string, description:string, longdescription:string, ano: number, coverurl: string, imgs: Array<string>) {
     this.id = id;
     this.nome = nome;
     this.description = description;
     this.longdescription = longdescription;
     this.ano = ano;
     this.coverurl = coverurl;
+    this.imgs = imgs;
   }
 }
