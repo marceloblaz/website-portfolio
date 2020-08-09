7fc05d2ab9e8b5f02cfcf2c5a790f67ee8e215d4
marceloblaz <marcelo.brasileirovale@gmail.com>
2020-08-09 18:29:43 -0300

    bug fixes

--- src/app/projects-service.service.ts
+++ src/app/projects-service.service.ts
@@ -20,11 +20,11 @@ export class ProjectsServiceService {
         "https://firebasestorage.googleapis.com/v0/b/mb-portfolio-e56f8.appspot.com/o/pagboletos-cover.png?alt=media&token=b8d3e313-b1a8-4a46-a7a1-66bc8bb5033c",
         [
           "https://firebasestorage.googleapis.com/v0/b/mb-portfolio-e56f8.appspot.com/o/pag-boletos-btn-animation.gif?alt=media&token=3cc18951-745d-4ac9-a582-fb73f241fdba",
+          "https://firebasestorage.googleapis.com/v0/b/mb-portfolio-e56f8.appspot.com/o/pagboletos-img-03.jpg?alt=media&token=9830c90a-d736-4cc7-874a-51ef74ff3a5a",
           "https://firebasestorage.googleapis.com/v0/b/mb-portfolio-e56f8.appspot.com/o/pag-boletos-anim-animation.gif?alt=media&token=68ac67d5-d544-444a-9ab0-4c896591b0ed",
-          "https://firebasestorage.googleapis.com/v0/b/mb-portfolio-e56f8.appspot.com/o/pag-boletos-no-internet-animation.gif?alt=media&token=b8d9cfd4-f81e-42fc-973a-aa755f741bf4",
-          "https://firebasestorage.googleapis.com/v0/b/mb-portfolio-e56f8.appspot.com/o/pagboletos-img-01.jpg?alt=media&token=6dff88d5-bc63-40cd-b675-2d525e035096",
           "https://firebasestorage.googleapis.com/v0/b/mb-portfolio-e56f8.appspot.com/o/pagboletos-img-02.jpg?alt=media&token=7f8931a7-3372-4b24-8923-339c5101704c",
-          "https://firebasestorage.googleapis.com/v0/b/mb-portfolio-e56f8.appspot.com/o/pagboletos-img-03.jpg?alt=media&token=9830c90a-d736-4cc7-874a-51ef74ff3a5a"
+          "https://firebasestorage.googleapis.com/v0/b/mb-portfolio-e56f8.appspot.com/o/pag-boletos-no-internet-animation.gif?alt=media&token=b8d9cfd4-f81e-42fc-973a-aa755f741bf4",
+          "https://firebasestorage.googleapis.com/v0/b/mb-portfolio-e56f8.appspot.com/o/pagboletos-img-01.jpg?alt=media&token=6dff88d5-bc63-40cd-b675-2d525e035096"
         ]
       )
     );
@@ -34,7 +34,6 @@ export class ProjectsServiceService {
         "Buquês são flores mortas",
         "Screen printed poster",
         "This poster was inspired in a song by a brazilian artist, Criolo. The phrase says that ’bouquets are dead flowers'. That sounded so interesting to me from the moment I first heard it, the conflict between the liveness e beauty of a bouquet and the fact that those flowers are actually dead. So I created this design and screen printed with either white or black it on colored paper.",
-
         2016,
         "https://firebasestorage.googleapis.com/v0/b/mb-portfolio-e56f8.appspot.com/o/bsfm%2Fbuques-sao-flores-mortas-layout.jpg?alt=media&token=e612e9af-718b-496d-8697-96dfcf42e10f",
         [
