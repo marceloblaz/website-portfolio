6dbb58e82baab105d8066dfc61be0f8ce75bcbe9
marceloblaz <marcelo.brasileirovale@gmail.com>
2020-08-10 10:18:40 -0300

    removi a lista do componente app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,10 @@ import {Dado} from '../dado';
   styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent implements OnInit {
-  lista: Array<Dado> = [];
 
-  ngOnInit(): void {
-    this.lista.push(new Dado(1, 'Exemplo 1', 'descricao projeto 1', 2020, 'https://firebasestorage.googleapis.com/v0/b/mb-portfolio-e56f8.appspot.com/o/pagboletos-cover.png?alt=media&token=b8d3e313-b1a8-4a46-a7a1-66bc8bb5033c'));
-    this.lista.push(new Dado(1, 'Nome do projeto', 'descricao projeto 2', 2019, 'endereco da imagem da capa'));
+  constructor() { }
+
+  ngOnInit() {
   }
+
 }
