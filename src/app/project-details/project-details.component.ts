ff967e51e838414599a757edc1d9758709d497b8
marceloblaz <marcelo.brasileirovale@gmail.com>
2022-08-03 15:12:40 -0300

    3 files modified

--- src/app/project-details/project-details.component.ts
+++ src/app/project-details/project-details.component.ts
@@ -1,27 +1,28 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { ProjectsServiceService } from "../projects-service.service";
-import { ActivatedRoute } from "@angular/router";
-import { Dado } from "../../dado";
+import { Component, OnInit, Input } from '@angular/core';
+import { ProjectsServiceService } from '../projects-service.service';
+import { ActivatedRoute } from '@angular/router';
+import { Dado } from '../../dado';
 
 @Component({
-  selector: "app-project-details",
-  templateUrl: "./project-details.component.html",
-  styleUrls: ["./project-details.component.scss"]
+  selector: 'app-project-details',
+  templateUrl: './project-details.component.html',
+  styleUrls: ['./project-details.component.scss'],
 })
 export class ProjectDetailsComponent implements OnInit {
   projeto: Dado;
 
-  constructor( public projects: ProjectsServiceService, private route: ActivatedRoute ) { }
+  constructor(
+    public projects: ProjectsServiceService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-      let idroute = params.get("id");
+    this.route.paramMap.subscribe((params) => {
+      let idroute = params.get('id');
 
-      this.projeto = this.projects.buscaListaProjetos().find(item => item.id == idroute);
-
+      this.projeto = this.projects
+        .buscaListaProjetos()
+        .find((item) => item.nome == idroute);
     });
-
-    
   }
-
 }
